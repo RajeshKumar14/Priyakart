a132f3079a4de2ccff7b0c071b283b4abde79a59
RajeshKumar14 <Rajesh Raj@192.168.100.26>
2017-03-16 14:18:31 -0800

    ddmf

---
+++ src/com/app/model/Seller.java
@@ -0,0 +1,11 @@
+package com.app.model;
+
+public class Seller {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobileNo;
+	public Location loc;
+                 
+}
